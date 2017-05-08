c890fdad0c23c71052722cf2a57b967372b983e7
ERVINA\Ervina Aprilia S <ervinaasuka@gmail.com>
2017-05-08 19:33:55 +0700

    hallo

--- app/src/main/java/com/proyekta/app/project_lafic/api/ApiClient.java
+++ app/src/main/java/com/proyekta/app/project_lafic/api/ApiClient.java
@@ -9,11 +9,9 @@ import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-
-
 public class ApiClient {
 
-    public static final String BASE_URL = "http://192.168.1.5/rest_server/index.php/";
+    public static final String BASE_URL = "http://192.168.43.239:8080/rest_server/index.php/";
 
     private static Retrofit.Builder builder =
             new Retrofit.Builder()
