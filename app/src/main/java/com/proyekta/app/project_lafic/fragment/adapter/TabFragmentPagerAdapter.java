763463e78d76047a8f51fa60c63cab50bdc37cf6
irvanyale <irvanyale14@gmail.com>
2017-05-13 13:31:44 +0700

    add tabadapter

---
+++ app/src/main/java/com/proyekta/app/project_lafic/fragment/adapter/TabFragmentPagerAdapter.java
@@ -0,0 +1,26 @@
+package com.proyekta.app.project_lafic.fragment.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by WINDOWS 10 on 13/05/2017.
+ */
+
+public class TabFragmentPagerAdapter extends FragmentPagerAdapter {
+    
+    public TabFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
