a758bd87d5f00f4f9a1c05b000862333ed51f694
irvanyale <irvanyale14@gmail.com>
2017-06-07 23:40:57 +0700

    Semangat Vin

--- app/src/main/java/com/proyekta/app/project_lafic/api/ApiClient.java
+++ app/src/main/java/com/proyekta/app/project_lafic/api/ApiClient.java
@@ -11,8 +11,10 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiClient {
 
-    public static final String BASE_URL = "http://192.168.1.8/rest_server/index.php/";
-    public static final String BASE_URL_FOTO = "http://192.168.1.8/rest_server/images/";
+    //public static final String BASE_URL = "http://192.168.1.8/rest_server/index.php/";
+    public static final String BASE_URL = "http://lafic.dhanifudin.com/";
+    //public static final String BASE_URL_FOTO = "http://192.168.1.8/rest_server/images/";
+    public static final String BASE_URL_FOTO = "http://lafic.dhanifudin.com/images/";
 
     private static Retrofit.Builder builder =
             new Retrofit.Builder()
